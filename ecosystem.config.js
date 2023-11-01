b53a04726b02f568a43e1b25bb648fae08685af6
jeremy <jeremcombe@hotmail.fr>
2023-11-01 01:29:03 +0100

    change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,13 @@
 module.exports = {
   apps: [
     {
-      script: "pages/index.js",
-      watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
+      script: "npm",
+      args: "start",
+      interpreter: "none",
+      name: "nextjs-app",
+      exec_mode: "cluster",
+      instances: 2,
+      watch: false,
     },
   ],
 
@@ -19,7 +20,7 @@ module.exports = {
       path: "/home/ubuntu",
       "pre-deploy-local": "", // Commande pré-déploiement locale
       "post-deploy":
-        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production", // Commandes post-déploiement
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production", // Commandes post-déploiement
       "pre-setup": "", // Commande pré-config du serveur
       ssh_options: "PasswordAuthentication=yes", // Option pour autoriser l'authentification par mot de passe
       key: "~/.ssh/id_rsa", // Chemin vers votre clé RSA privée
