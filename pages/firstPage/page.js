3d1fc306c8d7d80842ce3a1787055b6b4070dcba
jeremy <jeremcombe@hotmail.fr>
2023-11-14 11:38:01 +0100

    delete line

--- pages/firstPage/page.js
+++ pages/firstPage/page.js
@@ -7,7 +7,6 @@ const FirstPage = () => {
   return (
     <div className={styles.titleGame}>
       <h1 className={styles.title}>NFT Guessr</h1>
-      <div className={styles.line}></div>
       <p className={styles.subtitle}>
         Will you find the NFTs around the world?
       </p>
