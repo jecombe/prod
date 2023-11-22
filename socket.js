7cd7ae4dec76d451ecdb56b82a2e551f86eb812c
jeremy <jeremcombe@hotmail.fr>
2023-11-22 03:23:22 +0100

    fix

--- socket.js
+++ socket.js
@@ -3,10 +3,8 @@ import io from "socket.io-client";
 import store from "./utils/store";
 
 const socket = io("http://localhost:8000"); // Remplacez par l'URL de votre serveur Socket.io
-console.log("eeeeeeeeeeeeeeeeeeeeee", socket);
 socket.on("allToken", (message) => {
   // Dispatch une action Redux pour gérer le message
-  console.log("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQq", message);
   store.dispatch(receiveSocketMessage(message));
 });
 
