b09cc012e0e492cdf423ef8a8be73bca16edad87
jeremy <jeremcombe@hotmail.fr>
2024-01-29 17:15:41 +0100

    test deploiy

--- pages/game/Map2.js
+++ pages/game/Map2.js
@@ -1,18 +1,9 @@
-// import React, { useState, useRef } from "react";
-// import {
-//   MapContainer,
-//   TileLayer,
-//   Marker,
-//   Popup,
-//   useMapEvents,
-// } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
+import React, { useState, useRef } from "react";
+import dynamic from "next/dynamic";
 // import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { useMapEvents } from "react-leaflet";
 
-// const ICON = L.icon({
-//   iconUrl: "/nfts.png",
-//   iconSize: [32, 32],
-// });
 function LocationMarker({ handleMapClick }) {
   const [position, setPosition] = useState(null);
 
@@ -33,64 +24,6 @@ function LocationMarker({ handleMapClick }) {
     </Marker>
   );
 }
-// const TouchMap = ({ handleMapClick }) => {
-//   console.log(handleMapClick);
-//   const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 });
-//   const ZOOM_LEVEL = 3;
-//   const mapRef = useRef();
-
-//   const handleClick = (e) => {
-//     // Obtenir les coordonnées du clic
-//     const { lat, lng } = e.latlng;
-//     console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-//     if (onMapClick) {
-//       onMapClick({ lat, lng });
-//     }
-//   };
-
-//   return (
-//     <div className="container">
-//       <div className="row">
-//         <div className="col">
-//           <div className="container">
-//             <MapContainer
-//               center={center}
-//               zoom={ZOOM_LEVEL}
-//               ref={mapRef}
-//               style={{ height: "300px", width: "100%" }}
-//               //              onclick={handleClick} // Utilisez onclick au lieu de onClick
-//               onClick={handleClick} // Utilisez onClick au lieu de onclick
-//             >
-//               <TileLayer
-//                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//               />
-//               {/* <Marker position={[51.505, -0.09]} icon={ICON}>
-//                 <Popup>
-//                   A pretty CSS3 popup. <br /> Easily customizable.
-//                 </Popup>
-//               </Marker> */}
-//               <LocationMarker handleMapClick={handleMapClick} />
-//             </MapContainer>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TouchMap;
-
-import React, { useState, useRef } from "react";
-import dynamic from "next/dynamic";
-// import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import { useMapEvents } from "react-leaflet";
-
-// const ICON = L.icon({
-//   iconUrl: "/nfts.png",
-//   iconSize: [32, 32],
-// });
 
 const MapContainer = dynamic(
   () => import("react-leaflet").then((mod) => mod.MapContainer),
@@ -116,13 +49,6 @@ const Marker = dynamic(
 const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
   ssr: false,
 });
-
-// const LocationMarker = dynamic(
-//   () => import("./LocationMarker").then((mod) => mod.LocationMarker),
-//   {
-//     ssr: false,
-//   }
-// );
 
 const TouchMap = ({ handleMapClick }) => {
   const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 });
