6bb07d040f4024dacd96d8e14eeee0882dea796d
jeremy <jeremcombe@hotmail.fr>
2024-01-29 17:37:57 +0100

    test deploiy

--- components/Map2.js
+++ components/Map2.js
@@ -1,17 +1,25 @@
-"use client";
-
 import React, { useState, useRef } from "react";
-import dynamic from "next/dynamic";
-// import L from "leaflet";
-
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useMapEvents } from "react-leaflet";
+import L from "leaflet";
 
+const ICON = L.icon({
+  iconUrl: "/nfts.png",
+  iconSize: [32, 32],
+});
 function LocationMarker({ handleMapClick }) {
+  console.log("marker", handleMapClick);
   const [position, setPosition] = useState(null);
 
   const map = useMapEvents({
     click(e) {
+      console.log(e.latlng);
       setPosition(e.latlng);
       // map.flyTo(e.latlng, map.getZoom());
       //map.locate();
@@ -27,33 +35,8 @@ function LocationMarker({ handleMapClick }) {
     </Marker>
   );
 }
-
-const MapContainer = dynamic(
-  () => import("react-leaflet").then((mod) => mod.MapContainer),
-  {
-    ssr: false,
-  }
-);
-
-const TileLayer = dynamic(
-  () => import("react-leaflet").then((mod) => mod.TileLayer),
-  {
-    ssr: false,
-  }
-);
-
-const Marker = dynamic(
-  () => import("react-leaflet").then((mod) => mod.Marker),
-  {
-    ssr: false,
-  }
-);
-
-const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
-  ssr: false,
-});
-
 const TouchMap = ({ handleMapClick }) => {
+  console.log(handleMapClick);
   const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 });
   const ZOOM_LEVEL = 3;
   const mapRef = useRef();
@@ -61,8 +44,9 @@ const TouchMap = ({ handleMapClick }) => {
   const handleClick = (e) => {
     // Obtenir les coordonnées du clic
     const { lat, lng } = e.latlng;
-    if (handleMapClick) {
-      handleMapClick({ lat, lng });
+    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+    if (onMapClick) {
+      onMapClick({ lat, lng });
     }
   };
 
@@ -70,21 +54,27 @@ const TouchMap = ({ handleMapClick }) => {
     <div className="container">
       <div className="row">
         <div className="col">
-          {/* <div className="container">
+          <div className="container">
             <MapContainer
               center={center}
               zoom={ZOOM_LEVEL}
               ref={mapRef}
               style={{ height: "300px", width: "100%" }}
-              onClick={handleClick}
+              //              onclick={handleClick} // Utilisez onclick au lieu de onClick
+              onClick={handleClick} // Utilisez onClick au lieu de onclick
             >
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
+              {/* <Marker position={[51.505, -0.09]} icon={ICON}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker> */}
               <LocationMarker handleMapClick={handleMapClick} />
             </MapContainer>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
