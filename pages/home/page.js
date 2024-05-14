bafa589980ccf4e783487d39a1ea3ddbe2e04217
jeremy <jeremcombe@hotmail.fr>
2024-05-14 23:08:15 +0200

    alert prevent

--- pages/home/page.js
+++ pages/home/page.js
@@ -1,4 +1,4 @@
-"use client";
+import { useEffect } from "react";
 import styles from "./home.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
@@ -6,6 +6,13 @@ import Contents from "../content/contents";
 import FirstPage from "../firstPage/page";
 
 export default function HomeView() {
+  useEffect(() => {
+    // Afficher l'alerte lorsque le composant est monté
+    alert(
+      "NFTGuessr is in beta. The smart contract are subject to change as the game is in development."
+    );
+  }, []); // [] assure que cette fonction ne se déclenche qu'une seule fois, lorsque le composant est monté
+
   return (
     <div>
       <div className={styles.first}>
